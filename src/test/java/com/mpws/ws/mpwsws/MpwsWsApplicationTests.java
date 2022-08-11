758686d04f007fcc0577ea70b5046ea8c4c9b54a
Tanguy Camus <tanguy.camus2021@campus-eni.fr>
2022-08-11 15:20:49 +0200

    [MPWS-WS] create project

---
+++ src/test/java/com/mpws/ws/mpwsws/MpwsWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpws.ws.mpwsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpwsWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
